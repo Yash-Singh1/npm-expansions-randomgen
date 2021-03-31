9b89831ec9f6d5566bba6fbe386688c99d73836d
Yash Singh <saiansh2525@gmail.com>
2021-03-31 09:26:08 -0700

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# `npm-expansions-randomgen`
+
+Implementations of `npm-expansions` in `randomgen`.
+
+## Usage
+
+The code can be run at:
+
+https://orteil.dashnet.org/randomgen/?gen=https://raw.githubusercontent.com/Yash-Singh1/npm-expansions-randomgen/main/index.txt
+
+## Development
+
+Run:
+
+```sh
+npm run build
+```
+
+To generate the `txt` file with the `randomgen` code.
---
+++ generate.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+try {
+  fs.rmSync('index.txt');
+} catch {}
+
+fs.closeSync(fs.openSync('index.txt', 'w'));
+
+nouns = [
+  ...new Set(
+    require('nouns')
+      .nouns.map((noun) => `${noun[0].toUpperCase()}${noun.slice(1)}`)
+      .concat(eval('(()=>{' + fs.readFileSync('./8325491/nouns.js', 'utf8') + ';return nouns;})()'))
+  ),
+];
+
+const adverbs = require('./adverbs');
+
+adjectives = eval('(()=>{' + fs.readFileSync('./8325491/adjectives.js', 'utf8') + ';return adjectives;})()').map(
+  (adjective) => `${adjective.charAt(0).toUpperCase()}${adjective.slice(1)}`
+);
+
+function randomGenListForJSList(listName, list) {
+  return `$${listName}\n${list.join('\n')}`;
+}
+
+function filtererForLetter(letter) {
+  return function (word) {
+    return word.startsWith(letter);
+  };
+}
+
+function pushList(letter, partOfSpeech) {
+  fs.writeFileSync(
+    'index.txt',
+    fs.readFileSync('index.txt', 'utf8') +
+      '\n\n' +
+      randomGenListForJSList(
+        `${letter}${partOfSpeech}`,
+        (partOfSpeech === 'noun' ? nouns : partOfSpeech === 'adverb' ? adverbs : adjectives).filter(filtererForLetter(letter.toUpperCase()))
+      )
+  );
+}
+
+pushList('n', 'noun');
+pushList('n', 'adjective');
+pushList('n', 'adverb');
+pushList('p', 'noun');
+pushList('p', 'adjective');
+pushList('p', 'adverb');
+pushList('m', 'noun');
+pushList('m', 'adverb');
+
+fs.writeFileSync(
+  'index.txt',
+  fs.readFileSync('index.txt', 'utf8') +
+    `\n\n$phrase
+[nnoun]: [padjective] [mnoun]
+[nnoun]'s [padjective] [mnoun]
+[nadjective] [padjective] [mnoun]
+[nadjective] [pnoun] [madverb]
+[nadverb] [padjective] [mnoun]
+[nadjective] [padjective] [mnoun]
+`
+);
+
+fs.writeFileSync('index.txt', fs.readFileSync('index.txt', 'utf8').trim() + '\n');
